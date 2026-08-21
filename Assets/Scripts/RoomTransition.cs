4d1447a21aae7c1c0a6e6973553e54312f55ba17
Blaustrom28 <Findus.Frerichs@t-online.de>
2026-08-21 19:10:05 +0200

    ScriptFile

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class RoomTransition : MonoBehaviour
 {
-
+    
     [SerializeField] GameObject door;
     [SerializeField] GameObject room;
 
+
+    
     // Start is called before the first frame update
     void Start()
     {
         door = GameObject.Find("DoorTest");
-        room = GameObject.Find("RoomFloorTest"); 
+        room = GameObject.Find("RoomFloorTest");
+        
     }
 
+   
     // Update is called once per frame
     void Update()
     {
